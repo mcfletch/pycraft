ce0e44a125542f7898040ac7c235877d1e14929f
Mike C. Fletcher <mcfletch@vrplumber.com>
2020-04-21 23:35:12 -0400

    CHAT help, multiline responses, V helper, easier expose operation

--- scripts/chatcommands.py
+++ scripts/chatcommands.py
@@ -1,5 +1,6 @@
 """Expose commands via chat on a minecraft instance"""
 from mcpi import minecraft, block
+from mcpi.vec3 import Vec3
 import threading, logging, inspect, operator
 import re, time, ast
 import contextlib, functools
@@ -16,45 +17,67 @@ def locked():
     yield MC_LOCK
     MC_LOCK.release()
 
-class Event(object):
-    def __init__(self, listener, user, message, args, named):
-        self.listener = listener
-        self.user = user 
-        self.message = message 
-        self.args = args 
-        self.named = named
-    
+
+def V(*args,**named):
+    """Construct an mcpi Vec3 from a list, tuple or 3 values"""
+    if named:
+        return Vec3(*args,**named)
+    if len(args) == 1 and isinstance(args[0],(list,tuple,Vec3,np.ndarray)):
+        return Vec3(*args[0])
+    else:
+        return Vec3(*args)
 
 DEFAULT_NAMESPACE = {
     'sin': np.sin,
     'cos': np.cos,
     'arange': np.arange,
+    'range': lambda *args,**named: list(range(*args,**named)),
+    'pi': np.pi,
+    'block': block,
+    'int': int,
+    'float': float,
+    'str': str,
+    'bool': bool,
+    'sqrt': np.sqrt,
+    'V': V,
 }
 DEFAULT_COMMANDS = {}
-def expose(command_set=None):
+def expose(command_set=None,name=None):
+    """Registers a function as being externally callable"""
     command_set = command_set or DEFAULT_COMMANDS
     def wrapper(function):
-        function_name = function.__name__
-        signature = list(inspect.signature(function).parameters)
-        if not (signature and signature[0] == 'event'):
-            raise TypeError(
-                'Exposed functions should have a parameter `mc` as the first parameter: %s'%(
-                    function,
-                )
-            )
+        function_name = name or function.__name__
         command_set[function_name] = function
-        @functools.wraps(function)
-        def final_function(*args,**named):
-            result = function(*args,**named)
-            if result is not None:
-                log.debug('%s(*%r,**named) => %r', function_name,args,named)
-        return final_function
+        return function
     return wrapper
 
 @expose()
-def echo(event,message):
+def echo(message, *, user=None):
     """Return the message to the user"""
-    return f'{event.user}: {message}'
+    return f'{user}: {message}'
+@expose()
+def help(value):
+    import inspect
+    return inspect.getdoc(value)
+
+class Entity(object):
+    """Hold a reference to an entity id on the listener"""
+    def __init__(self, listener, id):
+        self.listener = listener
+        self.id = id 
+    def __str__(self):
+        return self.get_name()
+    def get_name(self):
+        return self.listener.get_entity_name(self.id)
+    def get_position(self):
+        return self.listener.get_entity_position(self.id)
+    def get_direction(self):
+        """On Java edition, get the direction the user is facing"""
+        return self.listener.get_entity_direction(self.id)
+    def set_position(self):
+        return self.listener.set_entity_position(self.id)
+    position = property(get_position,set_position)
+    direction = property(get_direction,)
 
 class ChatListener(object):
     wanted = True
@@ -73,6 +96,16 @@ class ChatListener(object):
                 )
                 log.debug("Entity %s => %s", entity, current)
         return current
+    def get_entity_position(self, entity):
+        """Get entity position"""
+        with locked():
+            return self.mc.entity.getPos(entity)
+    def get_entity_tile_position(self, entity):
+        with locked():
+            return self.mc.entity.getTilePos(entity)
+    def get_entity_direction(self, entity):
+        with locked():
+            return self.mc.entity.getDirection(entity)
 
     
     def poll(self):
@@ -85,13 +118,15 @@ class ChatListener(object):
                 if match:
                     response = self.interpret(message)
                     if response:
-                        self.mc.postToChat(str(response))
+                        formatted = str(response)
+                        for line in formatted.splitlines()[:6]:
+                            self.mc.postToChat('<Bot> %s'%(line,))
             if not messages:
                 time.sleep(1)
     
     def interpret(self,message):
         """Interpret a command from our queue"""
-        sender = self.get_entity_name(message.entityId)
+        sender = Entity(self,message.entityId)
         log.info("Call from %s", sender)
         try:
             # expr
@@ -104,34 +139,14 @@ class ChatListener(object):
                 log.debug("Not a call: %r", message.message)
                 raise TypeError("Not a function call")
             log.info('Top level call %s',ast.dump(call,True,True))
-            func = self.get_function(call,self.namespace)
-            args,named = self.get_call_args(call, DEFAULT_NAMESPACE)
-            event = Event(
-                self,
-                sender,
-                message,
-                args,
-                named,
-            )
-            result = func(event,*args,**named)
-            return result
-
-            # return f'Would call {function} with {args}'
-            # node = top.children[0]
-            # log.debug('Expression %s',node)
-            # for child in ast.iter_child_nodes(node):
-            #     log.debug('Node: %s',node)
-            #     if isinstance(child,ast.Call):
-            #         log.info(
-            #             "Call parsed to %s, %s", 
-            #             ast.get_source_segment(child.func),
-            #             ast.get_source_segment(child.args),
-            #         )
-            #     else:
-            #         print(child)
-        except Exception:
-            log.exception('Failed during parsing')
-            return 'Error'
+            namespace = DEFAULT_NAMESPACE.copy()
+            namespace.update(DEFAULT_COMMANDS)
+            namespace['event'] = message
+            namespace['user'] = sender
+            return self.interpret_call(call,namespace)
+        except Exception as err:
+            log.exception('Failed on %s', message.message)
+            return f'{sender}: {err} on {repr(message.message)}'
     def get_function(self, call, namespace):
         """Lookup a function in namespace for the given call"""
         func = call.func
@@ -139,7 +154,7 @@ class ChatListener(object):
             raise TypeError("Function not called by name")
         name = func.id 
         if name not in namespace:
-            raise NameError("I don't know the name %r: known %s"%( name, n))
+            raise NameError("I don't know the name %r: known %s"%( name, sorted(namespace.keys())))
         function = namespace.get(name)
         if not hasattr(function,'__call__'):
             raise NameError("Sorry, %r isn't a function, it is a %s"%(name,type(function)))
@@ -149,10 +164,16 @@ class ChatListener(object):
         for arg in call.args:
             value = self.interpret_expr(arg,namespace)
             args.append(value)
+        for keyword in call.keywords:
+            named[keyword.arg] = self.interpret_expr(keyword.value,namespace)
         return args, named
     def interpret_call(self, call, namespace):
         func = self.get_function(call,namespace)
         args,named = self.get_call_args(call, namespace)
+        if getattr(func,'__kwdefaults__',None):
+            for key,default in func.__kwdefaults__.items():
+                if key not in named and key in namespace:
+                    named[key] = namespace[key]
         return func(*args,**named)
 
     BINOP_TO_OPERATOR = {
@@ -178,6 +199,7 @@ class ChatListener(object):
             if arg.id in namespace:
                 return namespace[arg.id]
             raise NameError(arg.id)
+        
         elif isinstance(arg,ast.Subscript):
             value = self.interpret_expr(arg.value,namespace)
             if isinstance(arg.slice,ast.Index):
@@ -192,6 +214,15 @@ class ChatListener(object):
                 raise ValueError(
                     'Do not yet support extended indexing'
                 )
+        elif isinstance(arg,ast.Attribute):
+            parent = self.interpret_expr(arg.value,namespace)
+            key = arg.attr 
+            if key.startswith('_'):
+                raise ValueError("Attributes starting with _ are disallowed", key)
+            if isinstance(parent,dict):
+                return parent[key]
+            else:
+                return getattr(parent,key)
         elif isinstance(arg,ast.BinOp):
             left,op,right = arg.left,arg.op,arg.right
             first,second = (
@@ -203,10 +234,20 @@ class ChatListener(object):
         elif isinstance(arg,ast.Tuple):
             value = tuple([self.interpret_expr(a,namespace) for a in arg.elts])
             return value
+        elif isinstance(arg, ast.List):
+            value = [self.interpret_expr(a,namespace) for a in arg.elts]
+            return value
         elif isinstance(arg,ast.Call):
             return self.interpret_call(arg,namespace)
         else:
-            return ast.literal_eval(ast.Expression(body=arg))
+            try:
+                return ast.literal_eval(ast.Expression(body=arg))
+            except Exception:
+                raise ValueError(
+                    'Unsupported operation: %s'%(
+                        ast.dump(arg)
+                    )
+                )
 
 
 def main():
--- scripts/test_chatcommands.py
+++ scripts/test_chatcommands.py
@@ -2,12 +2,17 @@ import unittest
 import chatcommands
 import ast
 import numpy as np
+from mcpi import vec3, block
+class FakeUser(object):
+    position = vec3.Vec3(0,0,0)
 
 class TestChatCommands(unittest.TestCase):
     def setUp(self):
         self.listener = chatcommands.ChatListener(None)
+        self.namespace = chatcommands.DEFAULT_NAMESPACE.copy()
+        self.namespace.update(chatcommands.DEFAULT_COMMANDS)
+        self.namespace['user'] = FakeUser()
     def test_expressions(self):
-        namespace = chatcommands.DEFAULT_NAMESPACE
         for statement,expected in [
             ("'this'",'this'),
             ('23',23),
@@ -19,11 +24,59 @@ class TestChatCommands(unittest.TestCase):
             ('[1,2,3]',[1,2,3]),
             ('[1,2,3][2]',3),
             ('[1,2,3,4][::2]',[1,3]),
+            ('[sin(2)+1,3]',[np.sin(2)+1,3]),
+            ('block.AIR',chatcommands.block.AIR),
+            ('range(3)',list(range(3))),
+            ('user.position+V(0,1,0)',vec3.Vec3(0,1,0)),
+            ('help(block)',block.__doc__),
+            ('V(z=3)',vec3.Vec3(0,0,3)),
+            # ('[sin(x) for x in [1,2,3]]',[np.sin(x) for x in [1,2,3]]),
         ]:
             parsed = ast.parse(
                 statement,
                 'chat.py',
                 'eval',
             )
-            result = self.listener.interpret_expr(parsed, namespace)
+            print(ast.dump(parsed))
+            result = self.listener.interpret_expr(parsed, self.namespace)
             assert result==expected,(statement,result)
+    
+    def test_failures(self):
+        for statement,expected in [
+            ('[32',SyntaxError),
+            ('[int(i) for i in [1,2,3]]',ValueError),
+            ('[1,2,3].__module__',ValueError),
+        ]:
+            try:
+                parsed = ast.parse(
+                    statement,
+                    'chat.py',
+                    'eval',
+                )
+                print(ast.dump(parsed))
+                result = self.listener.interpret_expr(parsed, self.namespace)
+                assert False, "Should have raised an error"
+            except Exception as err:
+                assert isinstance(err, expected), (statement, err)
+    def test_V(self):
+        V = chatcommands.V
+        for i,(result,expected) in enumerate([
+            (
+                V([1,2,3]),
+                vec3.Vec3(1,2,3),
+            ),
+            (
+                V(1),
+                vec3.Vec3(1),
+            ),
+            (
+                V(z=1),
+                vec3.Vec3(0,0,1),
+            ),
+            (
+                V((1,2,3)),
+                vec3.Vec3(1,2,3),
+            ),
+        ]):
+            assert result==expected,(i,result,expected)
+        

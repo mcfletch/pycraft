9494e2d6ab0bb8f63e059c8497e393cfef44bbd2
Mike C. Fletcher <mcfletch@vrplumber.com>
2020-04-25 00:41:48 -0400

    Go back to a much simpler bulldozer impl

--- pycraft/bulldozer.py
+++ pycraft/bulldozer.py
@@ -1,4 +1,4 @@
-from mcpi import block, minecraft
+from mcpi import minecraft, vec3
 import numpy as np
 import logging
 log = logging.getLogger(__name__)
@@ -6,10 +6,10 @@ from . import expose
 from .expose import V
 from . import entity
 from . import uniqueblocks
-from . import lockedmc
+from . import blocks
 
 @expose.expose()
-def bulldoze(depth=10,width=6,height=2,material=block.AIR,*,user=None,mc=None):
+def bulldoze(depth=10,width=6,height=2,material=blocks.AIR,*,user=None,mc=None):
     """Set blocks ahead of the user to the given material
 
     This aligns the block of material with the direction
@@ -17,45 +17,32 @@ def bulldoze(depth=10,width=6,height=2,material=block.AIR,*,user=None,mc=None):
     is in front of you.
     """
     x,y,z = position = user.position
-    material = expose.resolve_name(
-        material,expose.blocks.BLOCK_NAMES,
-    )
+    material = blocks.Block.as_instance(material)
     direction = user.direction
-    with lockedmc.locked(mc):
-        for to_clear in uniqueblocks.unique_blocks_only(generate_blocks_ahead(
-            V(x,y+1,z),
-            direction,
-            depth,
-            width=width,
-            height=height,
-        )):
-            # log.info("Clearing: %s", to_clear)
-            mc.setBlock(*to_clear,material)
 
-def unit_vector(v):
-    length = np.linalg.norm(v)
-    if length:
-        v /= length
-    return v
-
-
-def generate_blocks_ahead(position,direction,depth,width,height,step=.25):
-    """Given a starting position generate point-cloud in direction"""
-    start = np.array(tuple(position),dtype='f')
-    forward = np.array(tuple(direction),dtype='f')
-    above = np.array([0,1,0],dtype='f')
-
-    if not np.allclose(forward, above):
-        cross = unit_vector(np.cross(forward,above))
-        above = unit_vector(np.cross(forward,cross))
+    dx,dy,dz = direction
+    if dx and dz:
+        adx = np.abs(dx)
+        adz = np.abs(dz)
+        if adx > adz:
+            if dx > 0:
+                forward = vec3.Vec3(1,0,0)
+            else:
+                forward = vec3.Vec3(-1,0,0)
+        else:
+            if dz > 0:
+                forward = vec3.Vec3(0,0,1)
+            else:
+                forward = vec3.Vec3(0,0,-1)
+        cross = vec3.Vec3(*tuple(forward)[::-1])
     else:
-        cross = np.array(0,0,1)
+        forward = vec3.Vec3(0,1,0)
+        cross = vec3.Vec3(1,0,0)
+    start = position+ forward + (-cross * (width//2))
+    stop = start + (cross*width) + (forward*depth) + (vec3.Vec3(0,1,0)*height)
 
-    start = start + forward
-    for f_step in np.arange(0.0,depth,step):
-        forward_start = start + (forward * step)
-        for c_step in np.arange(-((width)/2),((width)/2),step):
-            cross_base = forward_start + (cross * c_step)
-            for h_step in np.arange(1.0,height+1.0,step):
-                yield cross_base + (above*h_step)
-
+    mc.setBlocks(
+        *start,
+        *stop,
+        material
+    )

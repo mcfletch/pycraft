33e3369b79a994dfeef70db4b8d6969a86117627
Mike C. Fletcher <mcfletch@vrplumber.com>
2020-04-26 13:22:38 -0400

    Scrip to introspect server for supported entity ids and report unknown

---
+++ pycraft/generateentities.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+"""Query the server to pull all entity types/names"""
+from mcpi import minecraft, entity
+def main():
+    mc = minecraft.Minecraft.create()
+    ids = mc.getEntityTypes()
+    known = set()
+    for key,value in entity.__dict__.items():
+        if isinstance(value,entity.Entity):
+            known.add(value.id)
+    unknown = [id for id in ids if id.id not in known]
+    if unknown:
+        print('Entity types supported on server but not known: %r'%(unknown))
+if __name__ == "__main__":
+    main()
+    

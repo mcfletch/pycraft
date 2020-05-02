3cc9b0f319383fdf0e2c97be12f8eb29b270f214
Mike C. Fletcher <mcfletch@vrplumber.com>
2020-05-01 22:12:59 -0400

    Make the farm command a template('farm') call

--- pycraft/farm.py
+++ pycraft/farm.py
@@ -1,9 +1,9 @@
 from pycraft.expose import expose
 from mcpi import minecraft 
-from pycraft import blocks
-import os
+from pycraft import blocks, fuzzymatch
+import os, glob, logging 
+log = logging.getLogger(__name__)
 HERE=os.path.dirname(os.path.abspath(__file__)) 
-FARM=os.path.join(HERE,'THOMSONS B-DAY FARM - Sheet1.csv')
 legend={
     'S':'STONE',
     'F':'OAK_FENCE',
@@ -18,23 +18,35 @@ legend={
     'FUR':'FURNACE',
     'CT':'CRAFTING_TABLE',
 }
+TEMPLATES = {}
+def scan_templates():
+    """Look in the directory templates for all files named *.csv"""
+    pattern = os.path.join(HERE,'templates','*.csv')
+    log.debug("Scanning %s for templates", pattern)
+    for filename in glob.glob(pattern):
+        base = os.path.basename(filename)[:-4] # Take off the .csv
+        log.info("Found template: %r", base)
+        TEMPLATES[base] = filename
+scan_templates()
+TEMPLATE_NAMESPACE = fuzzymatch.FuzzyNamespace(TEMPLATES)
 
 @expose()
-def bday_farm(*,mc=None,position=None,user=None,file=FARM):
+def template(template='farm',position=None,*,mc=None,user=None):
     if position==None:
         position=user.position
     slices=[]
     slice=[]
     slices.append(slice)
-    for line in open(file):
-        collums=line.strip().split(',')
-        if collums[0]=='-':
+    filename = getattr( TEMPLATE_NAMESPACE, template)
+    for line in open(filename):
+        columns=line.strip().split(',')
+        if columns[0]=='-':
             print('slice')
             slice=[]
             slices.append(slice)
         else:
             t=[]
-            for block in collums:
+            for block in columns:
                 tb=legend[block]
                 t.append(tb)
             slice.append(t)
@@ -45,6 +57,7 @@ def bday_farm(*,mc=None,position=None,user=None,file=FARM):
             for xup,block in enumerate(row):
                 bee=getattr(blocks,block)
                 mc.setBlock(x+xup,y+yup,z+zup,bee)
+
 if __name__=='__main__':
     mc=minecraft.Minecraft.create('192.168.15.32')
-    bday_farm(mc,position=(-5,1,-67))
+    template('farm',position=(-5,1,-67),mc=mc,)

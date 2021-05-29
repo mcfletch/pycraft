2b322e60cc7b2d723c5e010c92efb4ab61b8ebca
Mike C. Fletcher <mcfletch@vrplumber.com>
2021-05-29 12:39:05 -0400

    Use expose for basically everthing that doesn't have an override

--- pycraft-server/PycraftServer/src/main/java/com/vrplumber/pycraft/bukkitserver/HandlerRegistry.java
+++ pycraft-server/PycraftServer/src/main/java/com/vrplumber/pycraft/bukkitserver/HandlerRegistry.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.bukkit.Art;
+import org.bukkit.Fluid;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
@@ -41,9 +43,6 @@ class HandlerRegistry implements IHandlerRegistry {
 
     static {
         handlers = new ArrayList<Class>();
-        handlers.add(EchoHandler.class);
-        handlers.add(WorldHandler.class);
-        handlers.add(SubscriptionHandler.class);
         // handlers.add(EntityHandler.class);
     }
 
@@ -82,20 +81,31 @@ class HandlerRegistry implements IHandlerRegistry {
 
     public void exposeClass(Class cls) {
         /* Expose the class, its interfaces and any return types it needs */
+        if (cls == null) {
+            return;
+        }
         String name = cls.getSimpleName();
         if (implementations.get(name) == null) {
             registerImplementation(cls.getSimpleName(), new GenericHandler(cls));
             for (Class interfaceClass : cls.getInterfaces()) {
-                exposeClass(interfaceClass);
+                if (interfaceClass.getPackage() != null
+                        && interfaceClass.getPackage().getName().startsWith("org.bukkit")) {
+                    exposeClass(interfaceClass);
+                }
             }
             for (java.lang.reflect.Method method : cls.getMethods()) {
                 Class returnType = method.getReturnType();
                 if (!returnType.isPrimitive()) {
-                    exposeClass(returnType);
+                    if (returnType.getPackage() != null && returnType.getPackage().getName().startsWith("org.bukkit")) {
+                        exposeClass(returnType);
+                    }
                 }
                 for (Class paramType : method.getParameterTypes()) {
                     if (!paramType.isPrimitive()) {
-                        exposeClass(paramType);
+                        if (paramType.getPackage() != null
+                                && paramType.getPackage().getName().startsWith("org.bukkit")) {
+                            exposeClass(paramType);
+                        }
                     }
                 }
             }
@@ -104,108 +114,92 @@ class HandlerRegistry implements IHandlerRegistry {
     }
 
     public void registerHandlers() {
-        exposeClass(Chest.class);
 
         for (EntityType entityType : EntityType.values()) {
-            GenericHandler handler = new GenericHandler(entityType.getEntityClass());
-            if (handler.cls != null) {
-                registerImplementation(handler.getMethod(), handler);
-            }
+            exposeClass(entityType.getEntityClass());
         }
-        registerImplementation("Player", new GenericHandler(Player.class));
-        registerImplementation("OfflinePlayer", new GenericHandler(OfflinePlayer.class));
-        registerImplementation("BlockData", new GenericHandler(BlockData.class));
-        registerImplementation("Block", new GenericHandler(Block.class));
-        registerImplementation("Material", new GenericHandler(Material.class));
-        registerImplementation("Location", new GenericHandler(Location.class));
-        registerImplementation("Vector", new GenericHandler(Vector.class));
-        registerImplementation("ItemStack", new GenericHandler(ItemStack.class));
-        registerImplementation("Inventory", new GenericHandler(Inventory.class));
-        registerImplementation("InventoryHolder", new GenericHandler(InventoryHolder.class));
-        registerImplementation("Enchantment", new GenericHandler(Enchantment.class));
-        registerImplementation("Entity", new GenericHandler(Entity.class));
-        registerImplementation("EntityType", new GenericHandler(EntityType.class));
-        registerImplementation("Server", new GenericHandler(Server.class));
-        registerImplementation("Biome", new GenericHandler(Biome.class));
-        registerImplementation("BlockFace", new GenericHandler(BlockFace.class));
-        registerImplementation("PistonMoveReaction", new GenericHandler(PistonMoveReaction.class));
+        exposeClass(Player.class);
+        exposeClass(OfflinePlayer.class);
+        exposeClass(BlockData.class);
+        exposeClass(Block.class);
+        exposeClass(Material.class);
+        exposeClass(Location.class);
+        exposeClass(Vector.class);
+        exposeClass(ItemStack.class);
+        exposeClass(Inventory.class);
+        exposeClass(InventoryHolder.class);
+        exposeClass(Enchantment.class);
+        exposeClass(Entity.class);
+        exposeClass(EntityType.class);
+        exposeClass(Server.class);
+        exposeClass(Biome.class);
+        exposeClass(BlockFace.class);
+        exposeClass(PistonMoveReaction.class);
+        exposeClass(Art.class);
+        exposeClass(Fluid.class);
+        exposeClass(Bamboo.class);
+        exposeClass(Bed.class);
+        exposeClass(Beehive.class);
+        exposeClass(Bell.class);
+        exposeClass(BrewingStand.class);
+        exposeClass(BubbleColumn.class);
+        exposeClass(Cake.class);
+        exposeClass(Campfire.class);
+        exposeClass(Chain.class);
+        exposeClass(Chest.class);
+        exposeClass(Cocoa.class);
+        exposeClass(CommandBlock.class);
+        exposeClass(Comparator.class);
+        exposeClass(CoralWallFan.class);
+        exposeClass(DaylightDetector.class);
+        exposeClass(Dispenser.class);
+        exposeClass(Door.class);
+        exposeClass(EnderChest.class);
+        exposeClass(EndPortalFrame.class);
+        exposeClass(Farmland.class);
+        exposeClass(Fence.class);
+        exposeClass(Fire.class);
+        exposeClass(Furnace.class);
+        exposeClass(Gate.class);
+        exposeClass(GlassPane.class);
+        exposeClass(Grindstone.class);
+        exposeClass(Hopper.class);
+        exposeClass(Jigsaw.class);
+        exposeClass(Jukebox.class);
+        exposeClass(Ladder.class);
+        exposeClass(Lantern.class);
+        exposeClass(Leaves.class);
+        exposeClass(Lectern.class);
+        exposeClass(NoteBlock.class);
+        exposeClass(Observer.class);
+        exposeClass(Piston.class);
+        exposeClass(PistonHead.class);
+        exposeClass(RedstoneRail.class);
+        exposeClass(RedstoneWallTorch.class);
+        exposeClass(RedstoneWire.class);
+        exposeClass(Repeater.class);
+        exposeClass(RespawnAnchor.class);
+        exposeClass(Sapling.class);
+        exposeClass(Scaffolding.class);
+        exposeClass(SeaPickle.class);
+        exposeClass(Sign.class);
+        exposeClass(Slab.class);
+        exposeClass(Snow.class);
+        exposeClass(Stairs.class);
+        exposeClass(StructureBlock.class);
+        exposeClass(Switch.class);
+        exposeClass(TechnicalPiston.class);
+        exposeClass(TNT.class);
+        exposeClass(TrapDoor.class);
+        exposeClass(Tripwire.class);
+        exposeClass(TripwireHook.class);
+        exposeClass(TurtleEgg.class);
+        exposeClass(Wall.class);
+        exposeClass(WallSign.class);
 
-        registerImplementation("Bamboo", new GenericHandler(Bamboo.class));
-        registerImplementation("Bed", new GenericHandler(Bed.class));
-        registerImplementation("Beehive", new GenericHandler(Beehive.class));
-        registerImplementation("Bell", new GenericHandler(Bell.class));
-        registerImplementation("BrewingStand", new GenericHandler(BrewingStand.class));
-        registerImplementation("BubbleColumn", new GenericHandler(BubbleColumn.class));
-        registerImplementation("Cake", new GenericHandler(Cake.class));
-        registerImplementation("Campfire", new GenericHandler(Campfire.class));
-        registerImplementation("Chain", new GenericHandler(Chain.class));
-        registerImplementation("Chest", new GenericHandler(Chest.class));
-        registerImplementation("Cocoa", new GenericHandler(Cocoa.class));
-        registerImplementation("CommandBlock", new GenericHandler(CommandBlock.class));
-        registerImplementation("Comparator", new GenericHandler(Comparator.class));
-        registerImplementation("CoralWallFan", new GenericHandler(CoralWallFan.class));
-        registerImplementation("DaylightDetector", new GenericHandler(DaylightDetector.class));
-        registerImplementation("Dispenser", new GenericHandler(Dispenser.class));
-        registerImplementation("Door", new GenericHandler(Door.class));
-        registerImplementation("EnderChest", new GenericHandler(EnderChest.class));
-        registerImplementation("EndPortalFrame", new GenericHandler(EndPortalFrame.class));
-        registerImplementation("Farmland", new GenericHandler(Farmland.class));
-        registerImplementation("Fence", new GenericHandler(Fence.class));
-        registerImplementation("Fire", new GenericHandler(Fire.class));
-        registerImplementation("Furnace", new GenericHandler(Furnace.class));
-        registerImplementation("Gate", new GenericHandler(Gate.class));
-        registerImplementation("GlassPane", new GenericHandler(GlassPane.class));
-        registerImplementation("Grindstone", new GenericHandler(Grindstone.class));
-        registerImplementation("Hopper", new GenericHandler(Hopper.class));
-        registerImplementation("Jigsaw", new GenericHandler(Jigsaw.class));
-        registerImplementation("Jukebox", new GenericHandler(Jukebox.class));
-        registerImplementation("Ladder", new GenericHandler(Ladder.class));
-        registerImplementation("Lantern", new GenericHandler(Lantern.class));
-        registerImplementation("Leaves", new GenericHandler(Leaves.class));
-        registerImplementation("Lectern", new GenericHandler(Lectern.class));
-        registerImplementation("NoteBlock", new GenericHandler(NoteBlock.class));
-        registerImplementation("Observer", new GenericHandler(Observer.class));
-        registerImplementation("Piston", new GenericHandler(Piston.class));
-        registerImplementation("PistonHead", new GenericHandler(PistonHead.class));
-        registerImplementation("RedstoneRail", new GenericHandler(RedstoneRail.class));
-        registerImplementation("RedstoneWallTorch", new GenericHandler(RedstoneWallTorch.class));
-        registerImplementation("RedstoneWire", new GenericHandler(RedstoneWire.class));
-        registerImplementation("Repeater", new GenericHandler(Repeater.class));
-        registerImplementation("RespawnAnchor", new GenericHandler(RespawnAnchor.class));
-        registerImplementation("Sapling", new GenericHandler(Sapling.class));
-        registerImplementation("Scaffolding", new GenericHandler(Scaffolding.class));
-        registerImplementation("SeaPickle", new GenericHandler(SeaPickle.class));
-        registerImplementation("Sign", new GenericHandler(Sign.class));
-        registerImplementation("Slab", new GenericHandler(Slab.class));
-        registerImplementation("Snow", new GenericHandler(Snow.class));
-        registerImplementation("Stairs", new GenericHandler(Stairs.class));
-        registerImplementation("StructureBlock", new GenericHandler(StructureBlock.class));
-        registerImplementation("Switch", new GenericHandler(Switch.class));
-        registerImplementation("TechnicalPiston", new GenericHandler(TechnicalPiston.class));
-        registerImplementation("TNT", new GenericHandler(TNT.class));
-        registerImplementation("TrapDoor", new GenericHandler(TrapDoor.class));
-        registerImplementation("Tripwire", new GenericHandler(Tripwire.class));
-        registerImplementation("TripwireHook", new GenericHandler(TripwireHook.class));
-        registerImplementation("TurtleEgg", new GenericHandler(TurtleEgg.class));
-        registerImplementation("Wall", new GenericHandler(Wall.class));
-        registerImplementation("WallSign", new GenericHandler(WallSign.class));
-
-        for (Class handlerCls : handlers) {
-            try {
-                MessageHandler handler = (MessageHandler) (handlerCls.getDeclaredConstructor().newInstance());
-                if (handler == null) {
-                    // getLogger().warning(String.format("Unable to create %s instance",
-                    // handlerCls.getName()));
-                    continue;
-                }
-                // getLogger().info(
-                // String.format("Registering command %s", handler.getMethod()));
-                registerImplementation(handler.getMethod(), handler);
-
-            } catch (Exception err) {
-                err.printStackTrace();
-            }
-        }
+        registerImplementation("World", new WorldHandler());
+        registerImplementation("echo", new EchoHandler());
+        registerImplementation("subscribe", new SubscriptionHandler());
     }
 
     public MessageHandler getHandler(String name) {

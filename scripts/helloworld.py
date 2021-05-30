04a82aff072d5ddc85e08b4b4d4aa983344bfeac
Mike C. Fletcher <mcfletch@vrplumber.com>
2021-05-30 09:42:09 -0400

    Update hello world script for current api

--- scripts/helloworld.py
+++ scripts/helloworld.py
@@ -1,79 +1,61 @@
 #! /usr/bin/env python3
-# Minecraft api reference
-#  https://www.stuffaboutcode.com/p/minecraft-api-reference.html
+# We use asynchronous operations to wait for the server
+# to complete the things we've asked it to do, asynchronous
+# functions have an "async" in front of them and they tell
+# the interpreter to wait for something to finish by using
+# the "await" keyword
+import asyncio
+import logging
+from pycraft.server import channel
 
-# Get the collection of code that the minecraft people
-# have given us that lets us talk to minecraft, the
-# "library" or "module" provides lots of functions for
-# manipulating the minecraft server.
-from mcpi import minecraft, block, connection
 
-# Create a "thing" which is a `minecraft.Minecraft` instance
-# which lets us talk to the local minecraft server
-mc = minecraft.Minecraft.create()
-# Call a "method" on the minecraft server that lets us
-# post a message to the chat window...
-mc.postToChat("Hello from python")
+async def hello_world():
+    # We'll get a connection (channel) to the server, here we're
+    # using the default (localhost) server and port
+    chan = channel.Channel()
+    # We're waiting for the connection to the server to open
+    await chan.open()
+    # Now we ask the PycraftServer plugin in the server to
+    # poke around and tell us what APIs are available. This
+    # process will take a while (seconds) and will send a huge
+    # gob of data (MBs) to us that describes what we can do
+    # with the server
+    await chan.introspect()
 
-# Ask the server for a list of identifiers (ids) that let us
-# talk about the users with the server. Internally the server
-# thinks of the user as a number, with their name just a thing
-# attached to that user-number
-# TODO: This command actually crashes (raises an exception)
-# rather than returning an empty list when there are no logged-in
-# users
-try:
-    users = mc.getPlayerEntityIds()
-except connection.RequestError:
-    users = []
-else:
-    # Call the function `print` to print the *list* of user ids that 
-    # are logged in the minecraft server tracks users, mobs and villagers 
-    # as things called "entities"
-    print(
-        "Users: %s are online"%(users)
-    )
-# *if* users is not empty, do the things that are "inside" the if
-# if there are more than one users logged in, then users will be
-# a list of integer values such as [23,45,55]
-if users:
-    # for every user in users, assign the variable `user` to that user id
-    # then do everything "inside" the for loop with `user` referring
-    # to that number (id)
-    for user in users:
-        # for this *particular* user, find their current position
-        position = mc.entity.getPos(user)
-        # position is now a Vector, which is a sequence of 3 floating-point values
-        # which defines a position in 3D space
-        # x => how far East/West
-        # y => how far up/down
-        # z => how far North/South
-        name = mc.entity.getName(user)
-        # name is now a string of characters which is the user's name
-        # as they entered it in their Minecraft client
-        print('User #%d %s at %s'%( user, name, position))
-        # Unpack the 3 values (x,y,z) of the position vector into separate values
-        # so that we can manipulate *just* one of them
-        x,y,z = position
-        # Set the position of the player to the same x and z, but 
-        # y increased by 1m (y is up-and-down)
-        # The effect of this is to make the user jump up 1m
-        mc.player.setPos(x,y+1,z)
-        # We are going to create a vein of diamond blocks
-        # starting 1m North of the player and stretching 100m
-        # North from there...
-        # For every number in the range 0-100 (not including the end)
-        # assign the number to the variable offset, then do this
-        # block of statements (so here we will do the statements 100 
-        # times)
-        for offset in range(100):
-            # offset is 0, then 1, then 2, then 3
-            # In minecraft you "create" a block by "setting"
-            # it to a particular block-type. Some block-types
-            # such as wood have sub-types like Oak, Pine, etc
-            mc.setBlock(
-                x,
-                y,
-                z+1+offset, # user's z (north/south) plus 1 plus [0,1,2,3,4,...,99]
-                block.DIAMOND_BLOCK.id # what we want to set it to...
-            )
+    from pycraft.server import world
+    from pycraft import acommands
+
+    # name= here can be any string, the server actually does have
+    # a name, but the api will ignore it because there's only
+    # one server on the far side of the connection...
+    server_api = world.Server(name='server')
+
+    # Again, we have to await the call, otherwise Python will
+    # never get around to running the operation...
+    await server_api.broadcastMessage("Hello from Python")
+
+    # Print out a list of every player that has ever connected
+    # to this server
+    # offline_players = await server_api.getOfflinePlayers()
+    # print("Known players")
+    # for player in offline_players:
+    #     print(f'{player.name}: {player.uuid}')
+    for world in await server_api.getWorlds():
+        print(f"In world: {world.name}")
+        print("Online players:")
+        for player in await world.getPlayers():
+            print(f'    {player.name}: {player.uuid} at {player.location}')
+            try:
+                await acommands.give('elytra', player=player)
+            except channel.MethodInvocationError as err:
+                print(f"  Could not give elytra to {player.name}: {err}")
+
+
+def main():
+    """This regular function just starts the asyncio event loop"""
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(hello_world())
+
+
+if __name__ == "__main__":
+    main()

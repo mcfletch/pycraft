8c3223189227e5cae11ac59199e37e2041ecff9e
Mike C. Fletcher <mcfletch@vrplumber.com>
2021-05-30 10:47:11 -0400

    Move the PycraftServer plugin to its own repository

--- pycraft-server/PycraftServer/build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash 
-
-mvn -e package
-cp target/PycraftServer-1.0-SNAPSHOT.jar ../../scratch-world/plugins/
-cp target/PycraftServer-1.0-SNAPSHOT.jar ../../moosh-world/plugins/
-cp target/PycraftServer-1.0-SNAPSHOT.jar ../../slime-world/plugins/
--- pycraft-server/PycraftServer/src/main/java/com/vrplumber/pycraft/bukkitserver/HelperMethod.java
+++
@@ -1,8 +0,0 @@
-package com.vrplumber.pycraft.bukkitserver;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface HelperMethod {
-}

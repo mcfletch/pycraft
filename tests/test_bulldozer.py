9aba02f45ef12c89e9c558767e2d646dba35ce62
Mike C. Fletcher <mcfletch@vrplumber.com>
2021-05-30 10:30:40 -0400

    Update the test suite to run with modern api

--- tests/test_bulldozer.py
+++ tests/test_bulldozer.py
@@ -2,20 +2,21 @@ import unittest
 from pycraft import bulldozer, uniqueblocks
 import ast
 import numpy as np
-from mcpi import vec3, block
+from pycraft.server import world
+
 
 class TestUniqueBlocks(unittest.TestCase):
     def test_unique(self):
-        duplicates = np.array([
-            [1.2,1,0],
-            [1.25,1,0],
-            [1.22,1.3,0],
-            [1.2,1,0.4],
-            [1.2,1.8,0],
-            [1.2,1.9,0.2],
-        ],dtype='f')
-        result = list(uniqueblocks.unique_blocks_only(
-            duplicates
-        ))
+        duplicates = np.array(
+            [
+                [1.2, 1, 0],
+                [1.25, 1, 0],
+                [1.22, 1.3, 0],
+                [1.2, 1, 0.4],
+                [1.2, 1.8, 0],
+                [1.2, 1.9, 0.2],
+            ],
+            dtype='f',
+        )
+        result = list(uniqueblocks.unique_blocks_only(duplicates))
         assert len(result) == 1, result
-    
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,126 +1,145 @@
 import unittest
-from pycraft import interpreter
-from pycraft import expose  
-from pycraft import blocks
-from pycraft import entity
-from pycraft import commands
+from pycraft import ainterpreter as interpreter
+from pycraft import expose
+from pycraft import acommands as commands
+from pycraft.server import world
+import pytest
 import ast
 import numpy as np
-from mcpi import vec3
+import uuid
+
+
+class FakeServer(object):
+    pass
+
+
+class FakeChannel(object):
+    server = FakeServer()
+
+
 class FakeUser(object):
-    position = vec3.Vec3(0,0,0)
+    position = world.Vector(0, 0, 0)
+    location = world.Location(['world', (0, 0, 0)])
+    name = 'MooUser'
+    uuid = uuid.uuid4()
+
+
 class FakeMessage(object):
     def __init__(self, attrs):
         self.__dict__.update(attrs)
+
+
 class FakeEntities(object):
     def get_entity_name(self, id):
         return 'Moo'
-    def get_entity_position(self, id):
-        return vec3.Vec3(42,42,42)
 
-class TestInterpreter(unittest.TestCase):
-    def setUp(self):
-        self.interpreter = interpreter.Interpreter(None)
-        self.namespace = self.interpreter.base_namespace()
-        self.namespace['user'] = FakeUser()
-        self.interpreter.entities = FakeEntities()
-    def test_expressions(self):
-        for statement,expected in [
-            ("'this'",'this'),
-            ('23',23),
-            ('(1,2,3)',(1,2,3)),
-            ('sin(3)',np.sin(3)),
-            ('cos(3)',np.cos(3)),
-            ('(1,2,3)+(3,4,5)',(1,2,3,3,4,5)),
-            ('"""those"""', 'those'),
-            ('[1,2,3]',[1,2,3]),
-            ('[1,2,3][2]',3),
-            ('[1,2,3,4][::2]',[1,3]),
-            ('[sin(2)+1,3]',[np.sin(2)+1,3]),
-            ('blocks.AIR',blocks.AIR),
-            ('entities.ENDER_C',expose.ENTITY_NAMESPACE.ENDER_CRYSTAL),
-            ('range(3)',np.arange(3)),
-            ('user.position+V(0,1,0)',vec3.Vec3(0,1,0)),
-            ('V(z=3)',vec3.Vec3(0,0,3)),
-            ('[x*2 for x in range(3)]',[0,2,4]),
-            ('[(z,y,x) for (x,y,z) in [(2,3,4)]]', [(4,3,2)]),
-            ('[(z,y,x,r) for (x,y,z) in [(2,3,4)] for r in [1,2]]', [(4,3,2,1),(4,3,2,2)]),
-            ('[x for y in range(3) for x in range(y)]',[0,0,1]),
-            ('tuple(x*2 for x in range(3))',(0,2,4)),
-            ('{ x:y for (x,y) in [(1,2),(3,4)]}',{1:2,3:4}),
-            ('{ x for (x,y) in [(1,2),(3,4)]}',{1,3}),
-            # ('[sin(x) for x in [1,2,3]]',[np.sin(x) for x in [1,2,3]]),
-        ]:
+    def get_entity_position(self, id):
+        return world.Vector(42, 42, 42)
+
+
+@pytest.fixture
+def channel_setup():
+    class Self:
+        pass
+
+    self = Self()
+    self.interpreter = interpreter.AInterpreter(FakeChannel())
+    self.namespace = self.interpreter.base_namespace(
+        sender=FakeUser(),
+    )
+    self.namespace['user'] = FakeUser()
+    return self
+
+
+@pytest.mark.asyncio
+async def test_expressions(channel_setup):
+    for statement, expected in [
+        ("'this'", 'this'),
+        ('23', 23),
+        ('(1,2,3)', (1, 2, 3)),
+        ('sin(3)', np.sin(3)),
+        ('cos(3)', np.cos(3)),
+        ('(1,2,3)+(3,4,5)', (1, 2, 3, 3, 4, 5)),
+        ('"""those"""', 'those'),
+        ('[1,2,3]', [1, 2, 3]),
+        ('[1,2,3][2]', 3),
+        ('[1,2,3,4][::2]', [1, 3]),
+        ('[sin(2)+1,3]', [np.sin(2) + 1, 3]),
+        ('range(3)', np.arange(3)),
+        ('[x*2 for x in range(3)]', [0, 2, 4]),
+        ('[(z,y,x) for (x,y,z) in [(2,3,4)]]', [(4, 3, 2)]),
+        (
+            '[(z,y,x,r) for (x,y,z) in [(2,3,4)] for r in [1,2]]',
+            [(4, 3, 2, 1), (4, 3, 2, 2)],
+        ),
+        ('[x for y in range(3) for x in range(y)]', [0, 0, 1]),
+        # Regression here, we used to support this...
+        ('tuple(x*2 for x in range(3))', (0, 2, 4)),
+        ('{ x:y for (x,y) in [(1,2),(3,4)]}', {1: 2, 3: 4}),
+        ('{ x for (x,y) in [(1,2),(3,4)]}', {1, 3}),
+        ('[sin(x) for x in [1,2,3]]', [np.sin(x) for x in [1, 2, 3]]),
+    ]:
+        parsed = ast.parse(
+            statement,
+            'chat.py',
+            'eval',
+        )
+        print(ast.dump(parsed))
+        try:
+            result = await channel_setup.interpreter.interpret_expr(
+                parsed, channel_setup.namespace
+            )
+        except Exception as err:
+            err.args += (statement,)
+            raise
+        if isinstance(expected, np.ndarray):
+            assert np.allclose(result, expected)
+        else:
+            assert result == expected, (statement, result)
+
+
+@pytest.mark.asyncio
+async def test_failures(channel_setup):
+    for statement, expected in [
+        ('[32', SyntaxError),
+        ('[1,2,3].__module__', ValueError),
+    ]:
+        try:
             parsed = ast.parse(
                 statement,
                 'chat.py',
                 'eval',
             )
             print(ast.dump(parsed))
-            result = self.interpreter.interpret_expr(parsed, self.namespace)
-            if isinstance(expected, np.ndarray):
-                assert np.allclose(result,expected)
-            else:
-                assert result==expected,(statement,result)
-    
-    def test_failures(self):
-        for statement,expected in [
-            ('[32',SyntaxError),
-            ('[1,2,3].__module__',ValueError),
-        ]:
-            try:
-                parsed = ast.parse(
-                    statement,
-                    'chat.py',
-                    'eval',
-                )
-                print(ast.dump(parsed))
-                result = self.interpreter.interpret_expr(parsed, self.namespace)
-                assert False, "Should have raised an error"
-            except Exception as err:
-                assert isinstance(err, expected), (statement, err)
-    def test_V(self):
-        V = commands.V
-        for i,(result,expected) in enumerate([
-            (
-                V([1,2,3]),
-                vec3.Vec3(1,2,3),
-            ),
-            (
-                V(1),
-                vec3.Vec3(1),
-            ),
-            (
-                V(z=1),
-                vec3.Vec3(0,0,1),
-            ),
-            (
-                V((1,2,3)),
-                vec3.Vec3(1,2,3),
-            ),
-        ]):
-            assert result==expected,(i,result,expected)
-    
-    def test_assignment(self):
-        message = FakeMessage({
+            result = await channel_setup.interpreter.interpret_expr(
+                parsed, channel_setup.namespace
+            )
+            assert False, "Should have raised an error"
+        except Exception as err:
+            assert isinstance(err, expected), (statement, err)
+
+
+@pytest.mark.asyncio
+async def test_assignment(channel_setup):
+    message = FakeMessage(
+        {
             'message': '3',
-            'entityId': 4,
+            'player': channel_setup.namespace['player'],
             'assignment': 'a',
-        })
-        self.interpreter.interpret(message)
-        assert self.interpreter.user_namespaces == {
-            4: {'a':3},
-        }
-        assert self.interpreter.interpret(FakeMessage({
-            'message': 'echo(a)',
-            'entityId': 4,
-        })) == 3
-    def test_resolve(self):
-        for name,expected in [
-            ('bed',blocks.BED),
-            ('CRACKED_STONE_BRICKS',blocks.CRACKED_STONE_BRICKS),
-            ('white concrete',blocks.WHITE_CONCRETE)
-        ]:
-            result = getattr(expose.BLOCK_NAMESPACE,name)
-            assert result == expected, (name,result)
-        
+        },
+    )
+    await channel_setup.interpreter.interpret(message)
+    assert channel_setup.interpreter.user_namespaces == {
+        channel_setup.namespace['player'].uuid: {'a': 3},
+    }
+    assert (
+        await channel_setup.interpreter.interpret(
+            FakeMessage(
+                {
+                    'message': 'echo(a)',
+                    'player': channel_setup.namespace['player'],
+                }
+            )
+        )
+        == 3
+    )
